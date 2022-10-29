d63df84adc64b7d21d4c12d16629fda258cafeeb
Ansh <96686986+Ansh-Kushwaha@users.noreply.github.com>
2022-10-29 17:50:45 +0530

    Update DoublyLinkedList.cpp

--- Lab Assignments/DoublyLinkedList.cpp
+++ Lab Assignments/DoublyLinkedList.cpp
@@ -145,3 +145,154 @@ node* insert(node *first, int pos, int x){
         return first;
     }
 }
+
+node *del(node *first, int pos, int &x){
+    if(first == NULL){
+        cout << "List is empty.\n";
+        return first;
+    }
+    int l = length(first);
+    if(pos<1 || pos>l){
+        cout << "Invalid position\n";
+        return first;
+    }
+    else{
+        if(pos == 1){
+            node *q = first;
+            x = q->data;
+            first = first->next;
+            first->prev = NULL;
+            delete q;
+            return first;
+        }
+        else if(pos==l){
+            node*q = first;
+            for(int i=1; i<pos-1; i++)
+                q = q->next;
+            node *r = q->next;
+            x = r->data;
+            r->prev = NULL;
+            q->next = NULL;
+            delete r;
+            return first;
+        }
+        else{
+            node *q = first;
+            for(int i=1; i<pos-1; i++)
+                q = q->next;
+            node *r = q->next;
+            x = r->data;
+            q->next = r->next;
+            node *tmp = r->next;
+            tmp->prev = q;
+            delete r;
+            return first;
+        }
+    }
+}
+
+void output(node *first){
+    if(first == NULL){
+        cout << "List is empty.\n";
+        return;
+    }
+    while(first != NULL){
+        cout << first->data << " ";
+        first = first->next;
+    }
+    cout << "\n";
+}
+
+int main(){
+    node *start = NULL;
+	int n, choice, x, k, t;
+	bool cont = true;
+	cout << "***************************\n";
+	cout << "Prepared by Ansh Kushwaha\n";
+	cout << "***************************\n";
+	while(cont){
+		cout << "1 : Create\n";
+		cout << "2 : Insert\n";
+		cout << "3 : Delete\n";
+		cout << "4 : isFull\n";
+		cout << "5 : isEmpty\n";
+		cout << "6 : length\n";
+		cout << "7 : Find\n";
+		cout << "8 : Search\n";
+		cout << "9 : Reverse\n";
+		cout << "10 : Output\n";
+		cout << "11 : Exit\n";
+		cout << "***************************\n";
+		cout << "Enter your choice : ";
+		cin >> choice;
+		cout << "\n";
+
+		switch(choice){
+			case 1:
+				cout << "Enter the size of list : ";
+				cin >> n;
+				start = create(start, n);
+				break;
+			case 2:
+				cout << "Enter the position you want to insert at : ";
+				cin >> k;
+				cout << "Enter the data you want to insert : ";
+				cin >> x;
+				start = insert(start, k, x);
+				break;
+			case 3:
+				cout << "Enter the position of the element you want to delete : ";
+				cin >> k;
+				start = del(start, k, x);
+				cout << "Deleted " << x << "\n";
+				break;
+			case 4:
+				if(isFull(start))
+					cout << "List is full.\n";
+				else
+					cout << "List is not full.\n";
+				break;
+			case 5:
+				if(isEmpty(start))
+					cout << "List is empty.\n";
+				else
+					cout << "List is not empty.\n";
+				break;
+			case 6 :
+				cout << "Length of list = " << length(start) << "\n";
+				break;
+			case 7:
+				cout << "Enter the position of element you want to find : ";
+				cin >> n;
+				t = find(start, n);
+				if(t == -9999)
+					break;
+				else
+					cout << "Element at " << n << " position = " << t << "\n";
+				break;
+			case 8:
+				cout << "Enter the element you want to search : ";
+				cin >> x;
+				t = search(start, x);
+				if(t == -9999)
+					break;
+				else
+					cout << "Found at " << t << " position.\n";
+				break;
+			case 9:
+				cout << "List reversed.\n";
+				break;
+			case 10:
+				cout << "\n";
+				output(start);
+				break;
+			case 11:
+				cout << "Exiting...\n";
+				cont = false;
+				break;
+			default: cout << "Incorrect choice.\n"; 
+		}
+		cout << "\n";
+	}
+    return 0;
+}
